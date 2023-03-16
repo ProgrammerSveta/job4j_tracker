581e466eef1961e397cc26ce027443497a1d6bca
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-03-16 11:59:17 +0300

    2. Перегрузка конструктора [#173890 #401339]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -1,7 +1,5 @@
 package ru.job4j.oop;
-
 public class Computer {
-
     private boolean multiMonitor;
     private int ssd;
     private String cpu;
@@ -9,11 +7,9 @@ public class Computer {
     public Computer() {
     }
 
-    public void printInfo() {
-        System.out.println("Много мониторов: " + multiMonitor);
-        System.out.println("SSD: " + ssd + " GB");
-        System.out.println("Модель CPU: " + cpu);
-        System.out.println();
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
     }
 
     public Computer(boolean multiMonitor, int ssd, String cpu) {
@@ -22,15 +18,17 @@ public class Computer {
         this.cpu = cpu;
     }
 
-    public Computer(int ssd, String cpu) {
-        this.ssd = ssd;
-        this.cpu = cpu;
-    }
-
     public Computer(boolean multiMonitor, double ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = (int) ssd;
         this.cpu = cpu;
+    }
+
+    public void printInfo() {
+        System.out.println("Много мониторов: " + multiMonitor);
+        System.out.println("SSD: " + ssd + " GB");
+        System.out.println("Модель CPU: " + cpu);
+        System.out.println();
     }
 
     public static void main(String[] args) {
