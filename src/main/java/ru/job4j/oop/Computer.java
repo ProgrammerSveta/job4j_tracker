280158d79ca84e461df6809445c389f6eb99d83d
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-03-16 08:21:30 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -12,6 +12,17 @@ public class Computer {
         this.cpu = cpu;
     }
 
+    public Computer(int ssd, String cpu) {
+        this.ssd = ssd;
+        this.cpu = cpu;
+    }
+
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
     public Computer() {
     }
 
@@ -19,6 +30,7 @@ public class Computer {
         System.out.println("Много мониторов: " + multiMonitor);
         System.out.println("SSD: " + ssd + " GB");
         System.out.println("Модель CPU: " + cpu);
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -28,5 +40,13 @@ public class Computer {
         comp.printInfo();
         Computer c = new Computer();
         c.printInfo();
+        Computer first = new Computer();
+        first.printInfo();
+        Computer second = new Computer(true, 500, "Intel Core I7-10700K");
+        second.printInfo();
+        Computer third = new Computer(256, "AMD Ryzen 5 3600");
+        third.printInfo();
+        Computer forth = new Computer(true, 512.0, "AMD Ryzen 7 3700X");
+        forth.printInfo();
     }
 }
