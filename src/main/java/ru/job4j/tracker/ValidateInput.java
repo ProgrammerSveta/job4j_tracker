85538bb2d5b38dba52d8519c50db37f9ee84a65e
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-04-04 11:07:13 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -31,13 +31,12 @@ public class ValidateInput implements Input {
         boolean invalid = true;
         int value = -1;
         do {
-            String rsl = in.askStr(question);
-            if (!isNumber(rsl)) {
+            try {
+                value = in.askInt(question);
+                invalid = false;
+            } catch (NumberFormatException nfe) {
                 System.out.println("Please enter valid data");
-                continue;
             }
-            value = Integer.parseInt(rsl);
-            invalid = false;
         } while (invalid);
         return value;
     }
