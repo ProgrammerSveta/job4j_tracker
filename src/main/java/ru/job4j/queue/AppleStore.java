de445128fcc230a11fe0f7de2639964874e2e501
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-04-23 06:27:07 +0300

    1. Интерфейс Queue [#504942]

--- src/main/java/ru/job4j/queue/AppleStore.java
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -16,7 +16,7 @@ public class AppleStore {
         for (int i = 1; i < count; i++) {
             queue.poll();
         }
-        return queue.peek().name();
+        return queue.poll().name();
     }
 
     public String getFirstUpsetCustomer() {
