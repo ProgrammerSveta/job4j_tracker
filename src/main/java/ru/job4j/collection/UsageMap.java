f2c1b3fee7b183b983159feefc1dcee6174bc693
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-04-26 06:49:40 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("petr@yandex.ru", "Petr Arsentev");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("arsentev@yandex.ru", "Petya Arsentev");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
