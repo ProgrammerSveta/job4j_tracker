a86e8f0179b4735411dd909eb120681334013aae
ProgrammerSveta <leshiarelaohk@gmail.com>
2023-06-06 13:42:53 +0300

    2.4. Отложенное исполнение лямбды [#249211 # [#249211]]

---
+++ src/main/java/ru/job4j/function/DeferredExecution.java
@@ -0,0 +1,28 @@
+package ru.job4j.function;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class DeferredExecution {
+
+    public static void main(String[] args) {
+        String[] name = {
+                "Ivan",
+        };
+        Comparator<String> lengthCmp = (left, right) -> {
+            System.out.println("execute comparator");
+            return Integer.compare(left.length(), right.length());
+        }; // этот код не запутится, так как у name один элемент, там нечего сравнивать
+        Arrays.sort(name, lengthCmp);
+
+        String[] names = {
+                "Ivan",
+                "Petr"
+        };
+        Comparator<String> lengthCmps = (left, right) -> {
+            System.out.println("execute comparator");
+            return Integer.compare(left.length(), right.length());
+        };
+        Arrays.sort(names, lengthCmps);
+    }
+}
